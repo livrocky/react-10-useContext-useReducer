5284c8cc29c47655442ea893c87e8b4d979f9549
rokas <rokasra@gmail.com>
2022-06-20 09:19:26 +0300

    max7 min0

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,6 +2,13 @@ import css from './Counter.module.css';
 import { useReducer, useState } from 'react';
 import Card from '../Card/Card';
 
+const ACTIONS = {
+  INCR: 'INCR',
+  DECR: 'DECR',
+  DOWN5: 'DOWN5',
+  UPBY: 'UPBY',
+};
+
 const initCounterValue = { count: 0 };
 
 function counterReducer(state, action) {
@@ -9,11 +16,22 @@ function counterReducer(state, action) {
   console.log('action ===', action);
 
   switch (action.type) {
-    case 'INCR':
+    case ACTIONS.INCR:
+      if (state.count === 7) {
+        const stateCopy1 = { ...state };
+        stateCopy1.error = 'Negalima didinti daugiau nei 7';
+        return stateCopy1;
+      }
       return { count: state.count + 1 };
     case 'UPBY':
       return { count: state.count + action.payload };
     case 'DECR':
+      if (state.count === 0) {
+        const stateCopy = { ...state };
+        stateCopy.error = 'Negalima neigiama reiksme';
+        return stateCopy;
+      }
+      // console.log('state===', state);
       return { count: state.count - 1 };
     case 'DOWN5':
       return { count: state.count - 5 };
@@ -29,7 +47,7 @@ function Counter() {
 
   function handleInc() {
     // setValue((prevValue) => prevValue + 1);
-    dispatch({ type: 'INCR' });
+    dispatch({ type: ACTIONS.INCR });
   }
   function handleDec() {
     // setValue((prevValue) => prevValue - 1);
@@ -38,13 +56,18 @@ function Counter() {
 
   // TODO: padaryti reset
 
+  // TODO: padaryti kad negalima didinti daugiau negu 7. Atvaizduoti klaida ir paslepti myktuka UP
+
   return (
     <div>
       <h2 className={css.count}>{state.count}</h2>
-      <button onClick={() => dispatch({ type: 'UPBY', payload: 7 })}>Up by 7</button>
-      <button onClick={handleInc}>Increase</button>
+      {/* patikrinti ar yra klaida, jeigu yra - atvaizduoti */}
+      {state.error && <h3>{state.error}</h3>}
+      {/* <button onClick={() => dispatch({ type: 'UPBY', payload: 7 })}>Up by 7</button> */}
+
+      {state.count < 7 && <button onClick={handleInc}>Increase</button>}
       <button onClick={handleDec}>Down</button>
-      <button onClick={() => dispatch({ type: 'DOWN5' })}>down 5</button>
+      {/* <button onClick={() => dispatch({ type: 'DOWN5' })}>down 5</button> */}
       <Card />
     </div>
   );

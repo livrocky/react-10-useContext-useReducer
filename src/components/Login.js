bc4c8688c0a868a671d6daf801ef4ce46606e0cc
rokas <rokasra@gmail.com>
2022-06-20 10:01:48 +0300

    input

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,42 @@
 // import css from 'Login.module.css';
 import AuthContext from './../store/authContext';
-import { useContext, useRef } from 'react';
+import { useContext, useRef, useReducer } from 'react';
+
+const initValues = {
+  username: '',
+  password: '',
+  password1: '',
+  usernameErr: '',
+  passwordErr: '',
+};
+
+function loginReducerFn(state, action) {
+  switch (action.type) {
+    case 'password':
+      if (state.username.length === 0) {
+        return { ...state, password: action.payload, usernameErr: 'Privalomas laukas' };
+      }
+      return { ...state, password: action.payload };
+    case 'username':
+      return { ...state, username: action.payload, usernameErr: '' };
+    case 'usernameBlur':
+      if (state.username.length === 0) {
+        return { ...state, usernameErr: 'Privalomas laukas' };
+      }
+      return state;
+    case 'input':
+      console.log(action.payload.name);
+      console.log(action.payload.value);
+      return { ...state, [action.payload.name]: action.payload.value };
+    default:
+      throw new Error('Invalid action type: ' + action.type);
+  }
+}
 
 function Login() {
+  // TODO: Login abu inputus valdom su vienu useReducer state
+  const [state, dispatch] = useReducer(loginReducerFn, initValues);
+
   const ctx = useContext(AuthContext);
 
   const userNameRef = useRef();
@@ -20,9 +54,11 @@ function Login() {
   return (
     <div>
       <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input ref={userNameRef} type='text' placeholder='username' />
-        <input type='password' placeholder='password' />
+      <form onSubmit={handleLogin} autoComplete={'off'}>
+        <input onChange={(e) => dispatch({ type: 'username', payload: e.target.value })} onBlur={() => dispatch({ type: 'usernameBlur' })} value={state.username} ref={userNameRef} type='text' placeholder='username' />
+        {state.usernameErr && <p className='error'>{state.usernameErr}</p>}
+        <input onChange={(e) => dispatch({ type: 'password', payload: e.target.value })} value={state.password} type='password' name='password' placeholder='password' />
+        <input onChange={(e) => dispatch({ type: 'input', payload: e.target })} value={state.password1} type='password' name='password1' placeholder='password' />
         <button type='submit'>Send</button>
       </form>
     </div>
